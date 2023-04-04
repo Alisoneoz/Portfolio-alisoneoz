ff79807f1376abee6bbbbd651bfdae8580314c82
alisoneoz <alisonestephanyoz@gmail.com>
2023-04-03 22:01:22 -0400

    Organize projects data in a separated folder

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,35 @@
+export const projectsData = [
+    {
+      title: "To-Do App with User Authentication",
+      description:
+        "Built with the MERN stack, this app allows users to access their accounts and create, edit, show, and delete their tasks entered into the app.",
+      image: "projectImages/todoApp.png",
+      link: "https://todo-mern-app-alisoneoz.netlify.app/",
+      repoLink: "https://github.com/Alisoneoz/todo-mern-app",
+    },
+    {
+      title: "Nutri-Converter",
+      description:
+        "Web App (with Spanish UI) to convert nutrition facts information (input by the user) to food portions in the diabetic exchange list.",
+      image: "projectImages/nutriConverter.png",
+      link: "https://nutri-convertidor-alison-estephany.netlify.app/",
+      repoLink: "https://github.com/Alisoneoz/Nutri-Converter-of-Food-portions-to-diabetic-exchanges-App",
+    },
+    {
+      title: "E-commerce Clothing Store",
+      description:
+        "E-commerce website created with the MERN stack, Redux Toolkit, Firebase for user authentication, and Stripe Checkout to handle payments.",
+      image: "projectImages/dragonEcommerce.png",
+      link: "https://dragon-ecommerce-store.netlify.app/",
+      repoLink: "https://github.com/Alisoneoz/Dragon-ecommerce",
+    },
+
+    {
+      title: "Interactive Calculator",
+      description:
+        "This app is built with ReactJS and the MathJS library to execute the arithmetical operations in the calculator.",
+      image: "projectImages/interactive-calculator.png",
+      link: "https://interactive-calculator-reactjs.netlify.app/",
+      repoLink: "https://github.com/Alisoneoz/interactive-calculator-reactjs",
+    },
+  ];
